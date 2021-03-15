5fed8e4e5693492faa28be2ce2f7696458e22ceb
AdityaShah9 <78752908+AdityaShah9@users.noreply.github.com>
2021-03-15 17:49:09 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -32,12 +32,15 @@ background(gardenImage);
   if(cat.x - mouse.x < (cat.width - mouse.width)/2){
    cat.velocityX = 0;
    cat.addAnimation("catLastImage",catImage3);
+   cat.x = 300;
+   cat.scale = 0.2;
    cat.changeAnimation("catLastImage");
    mouse.addAnimation("mouseLastImage",mouseImage3);
+   mouse.scale = 0.15;
    mouse.changeAnimation("mouseLastImage");
   }
    
-  text(mouseX + ',' + mouseY, 10,45);
+  //text(mouseX + ',' + mouseY, 10,45);
 
 drawSprites();
 }
@@ -48,6 +51,7 @@ function keyPressed(){
       cat.addAnimation("catRunning",catImage2);
       cat.changeAnimation("catRunning");
       mouse.addAnimation("mouseTeasing",mouseImage2);
+      mouse.frameDelay = 25;
       mouse.changeAnimation("mouseTeasing");
    }
   }
